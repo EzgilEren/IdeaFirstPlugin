8d8da0aafa67d3ab9ca7c6d769b28e9a6a588e69
Ezgi Fistikcioglu <fistikciogluezgi@gmail.com>
2022-01-13 16:36:24 +0300

    Completed the sample project using the Gradle plugin.

--- src/main/java/com/fistikciogluezgi/Huaweiplugin/SearchQuestionAction.java
+++ src/main/java/com/fistikciogluezgi/Huaweiplugin/SearchQuestionAction.java
@@ -12,45 +12,55 @@ import org.jetbrains.annotations.NonNls;
 
 import java.util.Locale;
 import java.util.Objects;
+import java.util.Optional;
 
 public class SearchQuestionAction extends AnAction {
     /**
      * Convert selected text to a URL friendly string.
+     *
      * @param event
      */
     @Override
-    public final void actionPerformed(AnActionEvent event)
-    {
+    public final void actionPerformed(AnActionEvent event) {
         Editor editor = event.getRequiredData(CommonDataKeys.EDITOR);
         CaretModel caretModel = editor.getCaretModel();
+        String selectedText = caretModel.getCurrentCaret().getSelectedText();
 
         // For searches from the editor, we should also get file type information
         // to help add scope to the search using the Huawei search syntax.
 
+        // Using of Plugin DevKit -> https://plugins.jetbrains.com/docs/intellij/getting-started.html?from=jetbrains.org#using-devkit
         @NonNls String languageTag = "";
         PsiFile file = event.getData(CommonDataKeys.PSI_FILE);
-        if(file != null)
-        {
+        if (file != null) {
             Language lang = event.getData(CommonDataKeys.PSI_FILE).getLanguage();
             languageTag = "+[" + lang.getDisplayName().toLowerCase(Locale.ROOT) + "]";
         }
 
         // The update method below is only called periodically so need
         // to be careful to check for selected text
-        if(caretModel.getCurrentCaret().hasSelection())
-        {
+        if (caretModel.getCurrentCaret().hasSelection()) {
             String query = Objects.requireNonNull(caretModel.getCurrentCaret().getSelectedText()).replace(' ', '+') + languageTag;
             BrowserUtil.browse("https://developer.huawei.com/consumer/en/doc/search?type=all&val=" + query);
         }
+
+        // Using of Gradle -> https://plugins.jetbrains.com/docs/intellij/getting-started.html?from=jetbrains.org#using-gradle
+        Optional<PsiFile> psiFile = Optional.ofNullable(event.getData(CommonDataKeys.PSI_FILE));
+        String typeTag = psiFile
+                .map(PsiFile::getFileElementType)
+                .map(lang -> "all")
+                .orElse("doc");
+
+        BrowserUtil.browse("https://developer.huawei.com/consumer/en/doc/search?type=" + typeTag + "&val=" + selectedText);
     }
 
     /**
      * Only make this action visible when text is selected.
+     *
      * @param event
      */
     @Override
-    public void update(AnActionEvent event)
-    {
+    public void update(AnActionEvent event) {
         final Editor editor = event.getRequiredData(CommonDataKeys.EDITOR);
         CaretModel caretModel = editor.getCaretModel();
         event.getPresentation().setEnabledAndVisible(caretModel.getCurrentCaret().hasSelection());
