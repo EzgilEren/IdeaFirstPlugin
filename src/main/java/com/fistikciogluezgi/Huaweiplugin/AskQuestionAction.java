6d1380b76184e5ae395e86bf0d25130f460f73e5
Ezgi Fistikcioglu <fistikciogluezgi@gmail.com>
2022-01-13 14:57:50 +0300

    completed the query search.

---
+++ src/main/java/com/fistikciogluezgi/Huaweiplugin/AskQuestionAction.java
@@ -0,0 +1,15 @@
+package com.fistikciogluezgi.Huaweiplugin;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class AskQuestionAction extends AnAction {
+
+    @Override
+    public final void actionPerformed(@NotNull AnActionEvent e) {
+        // We use the built-in BrowserUtil class because it handles all the nuances of opening a web page on different operating systems and browsers.
+        BrowserUtil.browse(" https://forums.developer.huawei.com/forumPortal/en/topicpost");
+    }
+}
