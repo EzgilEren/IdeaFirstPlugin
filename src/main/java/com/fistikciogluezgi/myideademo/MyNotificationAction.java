da510838aa31bb8c5b23200e6d10be37960045ea
Ezgi Fistikcioglu <fistikciogluezgi@gmail.com>
2022-01-13 09:08:45 +0300

    first commit

---
+++ src/main/java/com/fistikciogluezgi/myideademo/MyNotificationAction.java
@@ -0,0 +1,13 @@
+package com.fistikciogluezgi.myideademo;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class MyNotificationAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
